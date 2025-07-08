b5ea87873174747f4b6c79031898ad2292bbf0c2
yashsthr10 <yksuthar10@gmail.com>
2025-07-08 15:57:34 +0530

    fixed horizontal scrolling and navbar

--- script.js
+++ script.js
@@ -157,3 +157,12 @@
                 
             });
         });
+
+
+        const navToggle = document.querySelector('.nav-toggle');
+        const navMenu = document.querySelector('.nav-menu');
+        if (navToggle && navMenu) {
+            navToggle.addEventListener('click', function() {
+                navMenu.classList.toggle('active');
+            });
+        }
